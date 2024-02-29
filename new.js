2e45078fa7e876fbf028a94bd678c661e5162763
fahinurhaquefima <fahinurhaquef@gmail.com>
2024-02-29 12:45:52 +0600

    e-commerce Website design

---
+++ new.js
@@ -0,0 +1,456 @@
+let sport = document.getElementById('sport')
+let bag = document.getElementById('bag')
+let beauty = document.getElementById('beauty')
+let camara = document.getElementById('camara')
+let Cosmetics = document.getElementById('Cosmetics')
+let Jewellery = document.getElementById('Jewellery')
+let Watches = document.getElementById('Watches')
+let SmartPhones = document.getElementById('SmartPhones')
+let electronic = document.getElementById('electronic')
+let Toys = document.getElementById('Toys')
+let Automotive = document.getElementById('Automotive')
+let travel = document.getElementById('travel')
+let lamp =document.getElementById('lamp')
+
+
+
+sport.addEventListener('click', function(){
+  let sport1 = document.getElementById('sport1')
+  let bag1 = document.getElementById('bag1')
+  let beauty1 = document.getElementById('beauty1')
+  let camara1 = document.getElementById('camara1')
+  let Cosmetics1 = document.getElementById('Cosmetics1')
+  let Jewellery1 = document.getElementById('Jewellery1')
+  let Watches1 = document.getElementById('Watches1')
+  let SmartPhones1 = document.getElementById('SmartPhones1')
+  let electronic1 = document.getElementById('electronic1')
+  let Toys1 = document.getElementById('Toys1')
+  let Automotive1 = document.getElementById('Automotive1')
+  let travel1 = document.getElementById('travel1')
+  let lamp1 = document.getElementsByClassName('lamp1')
+
+
+  sport1.style.display = 'block'
+  bag1.style.display = 'none'
+  beauty1.style.display = 'none'
+  camara1.style.display = 'none'
+  Cosmetics1.style.display = 'none'
+  Jewellery1.style.display = 'none'
+  Watches1.style.display = 'none'
+  SmartPhones1.style.display = 'none'
+  electronic1.style.display = 'none'
+  Toys1.style.display = 'none'
+  Automotive1.style.display = 'none'
+  travel1.style.display = 'none'
+  lamp1.style.display = 'none'
+})
+
+
+
+bag.addEventListener('click', function(){
+  let sport1 = document.getElementById('sport1')
+  let bag1 = document.getElementById('bag1')
+  let beauty1 = document.getElementById('beauty1')
+  let camara1 = document.getElementById('camara1')
+  let Cosmetics1 = document.getElementById('Cosmetics1')
+  let Jewellery1 = document.getElementById('Jewellery1')
+  let Watches1 = document.getElementById('Watches1')
+  let SmartPhones1 = document.getElementById('SmartPhones1')
+  let electronic1 = document.getElementById('electronic1')
+  let Toys1 = document.getElementById('Toys1')
+  let Automotive1 = document.getElementById('Automotive1')
+  let travel1 = document.getElementById('travel1')
+  let lamp1 = document.getElementsByClassName('lamp1')
+
+
+  sport1.style.display = 'none'
+  bag1.style.display = 'block'
+  beauty1.style.display = 'none'
+  camara1.style.display = 'none'
+  Cosmetics1.style.display = 'none'
+  Jewellery1.style.display = 'none'
+  Watches1.style.display = 'none'
+  SmartPhones1.style.display = 'none'
+  electronic1.style.display = 'none'
+  Toys1.style.display = 'none'
+  Automotive1.style.display = 'none'
+  travel1.style.display = 'none'
+  lamp1.style.display = 'none'
+})
+
+
+
+beauty.addEventListener('click', function(){
+  let sport1 = document.getElementById('sport1')
+  let bag1 = document.getElementById('bag1')
+  let beauty1 = document.getElementById('beauty1')
+  let camara1 = document.getElementById('camara1')
+  let Cosmetics1 = document.getElementById('Cosmetics1')
+  let Jewellery1 = document.getElementById('Jewellery1')
+  let Watches1 = document.getElementById('Watches1')
+  let SmartPhones1 = document.getElementById('SmartPhones1')
+  let electronic1 = document.getElementById('electronic1')
+  let Toys1 = document.getElementById('Toys1')
+  let Automotive1 = document.getElementById('Automotive1')
+  let travel1 = document.getElementById('travel1')
+  let lamp1 = document.getElementsByClassName('lamp1')
+
+
+  sport1.style.display = 'none'
+  bag1.style.display = 'none'
+  beauty1.style.display = 'block'
+  camara1.style.display = 'none'
+  Cosmetics1.style.display = 'none'
+  Jewellery1.style.display = 'none'
+  Watches1.style.display = 'none'
+  SmartPhones1.style.display = 'none'
+  electronic1.style.display = 'none'
+  Toys1.style.display = 'none'
+  Automotive1.style.display = 'none'
+  travel1.style.display = 'none'
+  lamp1.style.display = 'none'
+})
+
+
+
+camara.addEventListener('click', function(){
+  let sport1 = document.getElementById('sport1')
+  let bag1 = document.getElementById('bag1')
+  let beauty1 = document.getElementById('beauty1')
+  let camara1 = document.getElementById('camara1')
+  let Cosmetics1 = document.getElementById('Cosmetics1')
+  let Jewellery1 = document.getElementById('Jewellery1')
+  let Watches1 = document.getElementById('Watches1')
+  let SmartPhones1 = document.getElementById('SmartPhones1')
+  let electronic1 = document.getElementById('electronic1')
+  let Toys1 = document.getElementById('Toys1')
+  let Automotive1 = document.getElementById('Automotive1')
+  let travel1 = document.getElementById('travel1')
+  let lamp1 = document.getElementsByClassName('lamp1')
+
+
+  sport1.style.display = 'none'
+  bag1.style.display = 'none'
+  beauty1.style.display = 'none'
+  camara1.style.display = 'block'
+  Cosmetics1.style.display = 'none'
+  Jewellery1.style.display = 'none'
+  Watches1.style.display = 'none'
+  SmartPhones1.style.display = 'none'
+  electronic1.style.display = 'none'
+  Toys1.style.display = 'none'
+  Automotive1.style.display = 'none'
+  travel1.style.display = 'none'
+  lamp1.style.display = 'none'
+})
+
+
+
+
+
+Cosmetics.addEventListener('click', function(){
+  let sport1 = document.getElementById('sport1')
+  let bag1 = document.getElementById('bag1')
+  let beauty1 = document.getElementById('beauty1')
+  let camara1 = document.getElementById('camara1')
+  let Cosmetics1 = document.getElementById('Cosmetics1')
+  let Jewellery1 = document.getElementById('Jewellery1')
+  let Watches1 = document.getElementById('Watches1')
+  let SmartPhones1 = document.getElementById('SmartPhones1')
+  let electronic1 = document.getElementById('electronic1')
+  let Toys1 = document.getElementById('Toys1')
+  let Automotive1 = document.getElementById('Automotive1')
+  let travel1 = document.getElementById('travel1')
+  let lamp1 = document.getElementsByClassName('lamp1')
+
+
+  sport1.style.display = 'none'
+  bag1.style.display = 'none'
+  beauty1.style.display = 'none'
+  camara1.style.display = 'none'
+  Cosmetics1.style.display = 'block'
+  Jewellery1.style.display = 'none'
+  Watches1.style.display = 'none'
+  SmartPhones1.style.display = 'none'
+  electronic1.style.display = 'none'
+  Toys1.style.display = 'none'
+  Automotive1.style.display = 'none'
+  travel1.style.display = 'none'
+  lamp1.style.display = 'none'
+})
+
+
+
+
+
+Jewellery.addEventListener('click', function(){
+  let sport1 = document.getElementById('sport1')
+  let bag1 = document.getElementById('bag1')
+  let beauty1 = document.getElementById('beauty1')
+  let camara1 = document.getElementById('camara1')
+  let Cosmetics1 = document.getElementById('Cosmetics1')
+  let Jewellery1 = document.getElementById('Jewellery1')
+  let Watches1 = document.getElementById('Watches1')
+  let SmartPhones1 = document.getElementById('SmartPhones1')
+  let electronic1 = document.getElementById('electronic1')
+  let Toys1 = document.getElementById('Toys1')
+  let Automotive1 = document.getElementById('Automotive1')
+  let travel1 = document.getElementById('travel1')
+  let lamp1 = document.getElementsByClassName('lamp1')
+
+
+  sport1.style.display = 'none'
+  bag1.style.display = 'none'
+  beauty1.style.display = 'none'
+  camara1.style.display = 'none'
+  Cosmetics1.style.display = 'none'
+  Jewellery1.style.display = 'block'
+  Watches1.style.display = 'none'
+  SmartPhones1.style.display = 'none'
+  electronic1.style.display = 'none'
+  Toys1.style.display = 'none'
+  Automotive1.style.display = 'none'
+  travel1.style.display = 'none'
+  lamp1.style.display = 'none'
+})
+
+
+
+
+
+Watches.addEventListener('click', function(){
+  let sport1 = document.getElementById('sport1')
+  let bag1 = document.getElementById('bag1')
+  let beauty1 = document.getElementById('beauty1')
+  let camara1 = document.getElementById('camara1')
+  let Cosmetics1 = document.getElementById('Cosmetics1')
+  let Jewellery1 = document.getElementById('Jewellery1')
+  let Watches1 = document.getElementById('Watches1')
+  let SmartPhones1 = document.getElementById('SmartPhones1')
+  let electronic1 = document.getElementById('electronic1')
+  let Toys1 = document.getElementById('Toys1')
+  let Automotive1 = document.getElementById('Automotive1')
+  let travel1 = document.getElementById('travel1')
+  let lamp1 = document.getElementsByClassName('lamp1')
+
+
+  sport1.style.display = 'none'
+  bag1.style.display = 'none'
+  beauty1.style.display = 'none'
+  camara1.style.display = 'none'
+  Cosmetics1.style.display = 'none'
+  Jewellery1.style.display = 'none'
+  Watches1.style.display = 'block'
+  SmartPhones1.style.display = 'none'
+  electronic1.style.display = 'none'
+  Toys1.style.display = 'none'
+  Automotive1.style.display = 'none'
+  travel1.style.display = 'none'
+  lamp1.style.display = 'none'
+})
+
+
+
+
+SmartPhones.addEventListener('click', function(){
+  let sport1 = document.getElementById('sport1')
+  let bag1 = document.getElementById('bag1')
+  let beauty1 = document.getElementById('beauty1')
+  let camara1 = document.getElementById('camara1')
+  let Cosmetics1 = document.getElementById('Cosmetics1')
+  let Jewellery1 = document.getElementById('Jewellery1')
+  let Watches1 = document.getElementById('Watches1')
+  let SmartPhones1 = document.getElementById('SmartPhones1')
+  let electronic1 = document.getElementById('electronic1')
+  let Toys1 = document.getElementById('Toys1')
+  let Automotive1 = document.getElementById('Automotive1')
+  let travel1 = document.getElementById('travel1')
+  let lamp1 = document.getElementsByClassName('lamp1')
+
+
+  sport1.style.display = 'none'
+  bag1.style.display = 'none'
+  beauty1.style.display = 'none'
+  camara1.style.display = 'none'
+  Cosmetics1.style.display = 'none'
+  Jewellery1.style.display = 'none'
+  Watches1.style.display = 'none'
+  SmartPhones1.style.display = 'block'
+  electronic1.style.display = 'none'
+  Toys1.style.display = 'none'
+  Automotive1.style.display = 'none'
+  travel1.style.display = 'none'
+  lamp1.style.display = 'none'
+})
+
+
+
+
+electronic.addEventListener('click', function(){
+  let sport1 = document.getElementById('sport1')
+  let bag1 = document.getElementById('bag1')
+  let beauty1 = document.getElementById('beauty1')
+  let camara1 = document.getElementById('camara1')
+  let Cosmetics1 = document.getElementById('Cosmetics1')
+  let Jewellery1 = document.getElementById('Jewellery1')
+  let Watches1 = document.getElementById('Watches1')
+  let SmartPhones1 = document.getElementById('SmartPhones1')
+  let electronic1 = document.getElementById('electronic1')
+  let Toys1 = document.getElementById('Toys1')
+  let Automotive1 = document.getElementById('Automotive1')
+  let travel1 = document.getElementById('travel1')
+  let lamp1 = document.getElementsByClassName('lamp1')
+
+
+  sport1.style.display = 'none'
+  bag1.style.display = 'none'
+  beauty1.style.display = 'none'
+  camara1.style.display = 'none'
+  Cosmetics1.style.display = 'none'
+  Jewellery1.style.display = 'none'
+  Watches1.style.display = 'none'
+  SmartPhones1.style.display = 'none'
+  electronic1.style.display = 'block'
+  Toys1.style.display = 'none'
+  Automotive1.style.display = 'none'
+  travel1.style.display = 'none'
+  lamp1.style.display = 'none'
+})
+
+
+
+
+Toys.addEventListener('click', function(){
+  let sport1 = document.getElementById('sport1')
+  let bag1 = document.getElementById('bag1')
+  let beauty1 = document.getElementById('beauty1')
+  let camara1 = document.getElementById('camara1')
+  let Cosmetics1 = document.getElementById('Cosmetics1')
+  let Jewellery1 = document.getElementById('Jewellery1')
+  let Watches1 = document.getElementById('Watches1')
+  let SmartPhones1 = document.getElementById('SmartPhones1')
+  let electronic1 = document.getElementById('electronic1')
+  let Toys1 = document.getElementById('Toys1')
+  let Automotive1 = document.getElementById('Automotive1')
+  let travel1 = document.getElementById('travel1')
+  let lamp1 = document.getElementsByClassName('lamp1')
+
+
+  sport1.style.display = 'none'
+  bag1.style.display = 'none'
+  beauty1.style.display = 'none'
+  camara1.style.display = 'none'
+  Cosmetics1.style.display = 'none'
+  Jewellery1.style.display = 'none'
+  Watches1.style.display = 'none'
+  SmartPhones1.style.display = 'none'
+  electronic1.style.display = 'none'
+  Toys1.style.display = 'block'
+  Automotive1.style.display = 'none'
+  travel1.style.display = 'none'
+  lamp1.style.display = 'none'
+})
+
+Automotive.addEventListener('click', function(){
+  let sport1 = document.getElementById('sport1')
+  let bag1 = document.getElementById('bag1')
+  let beauty1 = document.getElementById('beauty1')
+  let camara1 = document.getElementById('camara1')
+  let Cosmetics1 = document.getElementById('Cosmetics1')
+  let Jewellery1 = document.getElementById('Jewellery1')
+  let Watches1 = document.getElementById('Watches1')
+  let SmartPhones1 = document.getElementById('SmartPhones1')
+  let electronic1 = document.getElementById('electronic1')
+  let Toys1 = document.getElementById('Toys1')
+  let Automotive1 = document.getElementById('Automotive1')
+  let travel1 = document.getElementById('travel1')
+  let lamp1 = document.getElementsByClassName('lamp1')
+
+
+  sport1.style.display = 'none'
+  bag1.style.display = 'none'
+  beauty1.style.display = 'none'
+  camara1.style.display = 'none'
+  Cosmetics1.style.display = 'none'
+  Jewellery1.style.display = 'none'
+  Watches1.style.display = 'none'
+  SmartPhones1.style.display = 'none'
+  electronic1.style.display = 'none'
+  Toys1.style.display = 'none'
+  Automotive1.style.display = 'block'
+  travel1.style.display = 'none'
+  lamp1.style.display = 'none'
+})
+
+
+
+travel.addEventListener('click', function(){
+  let sport1 = document.getElementById('sport1')
+  let bag1 = document.getElementById('bag1')
+  let beauty1 = document.getElementById('beauty1')
+  let camara1 = document.getElementById('camara1')
+  let Cosmetics1 = document.getElementById('Cosmetics1')
+  let Jewellery1 = document.getElementById('Jewellery1')
+  let Watches1 = document.getElementById('Watches1')
+  let SmartPhones1 = document.getElementById('SmartPhones1')
+  let electronic1 = document.getElementById('electronic1')
+  let Toys1 = document.getElementById('Toys1')
+  let Automotive1 = document.getElementById('Automotive1')
+  let travel1 = document.getElementById('travel1')
+  let lamp1 = document.getElementsByClassName('lamp1')
+
+
+  sport1.style.display = 'none'
+  bag1.style.display = 'none'
+  beauty1.style.display = 'none'
+  camara1.style.display = 'none'
+  Cosmetics1.style.display = 'none'
+  Jewellery1.style.display = 'none'
+  Watches1.style.display = 'none'
+  SmartPhones1.style.display = 'none'
+  electronic1.style.display = 'none'
+  Toys1.style.display = 'none'
+  Automotive1.style.display = 'none'
+  travel1.style.display = 'block'
+  lamp1.style.display = 'none'
+})
+
+
+
+
+lamp.addEventListener('click', function(){
+  let lamp1 = document.getElementsByClassName('lamp1')
+  let sport1 = document.getElementById('sport1')
+  let bag1 = document.getElementById('bag1')
+  let beauty1 = document.getElementById('beauty1')
+  let camara1 = document.getElementById('camara1')
+  let Cosmetics1 = document.getElementById('Cosmetics1')
+  let Jewellery1 = document.getElementById('Jewellery1')
+  let Watches1 = document.getElementById('Watches1')
+  let SmartPhones1 = document.getElementById('SmartPhones1')
+  let electronic1 = document.getElementById('electronic1')
+  let Toys1 = document.getElementById('Toys1')
+  let Automotive1 = document.getElementById('Automotive1')
+  let travel1 = document.getElementById('travel1')
+ 
+
+  lamp1.style.display = 'block'
+  sport1.style.display = 'none'
+  bag1.style.display = 'none'
+  beauty1.style.display = 'none'
+  camara1.style.display = 'none'
+  Cosmetics1.style.display = 'none'
+  Jewellery1.style.display = 'none'
+  Watches1.style.display = 'none'
+  SmartPhones1.style.display = 'none'
+  electronic1.style.display = 'none'
+  Toys1.style.display = 'none'
+  Automotive1.style.display = 'none'
+  travel1.style.display = 'none'
+  
+})
+
+
+
+
+
